50204243da157a967e77b3267d4b6f34c4847a2a
Callum Begley <cfbegley7@gmail.com>
2025-07-15 12:23:29 +1200

    camera follow

--- Assets/bitsyScript.cs
+++ Assets/bitsyScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class bitsyScript : MonoBehaviour
 {
+    public float moveSpeed = 30f;
+    public Camera followCamera;
+    public float cameraFollowSpeed = 2f;
+    public Vector3 cameraOffset = new Vector3(0, 0, -10);
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,27 @@ public class bitsyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Move left
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            // Flip character to face left
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
         
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            // Move right
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            // Flip character to face right
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        
+        // Camera follow
+        if (followCamera != null)
+        {
+            Vector3 targetPosition = transform.position + cameraOffset;
+            followCamera.transform.position = Vector3.Lerp(followCamera.transform.position, targetPosition, cameraFollowSpeed * Time.deltaTime);
+        }
     }
 }
